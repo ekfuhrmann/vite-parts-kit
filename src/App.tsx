1149eff95a843459d132373540968992170f3d84
Eric <ekfuhrmann@gmail.com>
2022-09-29 15:59:37 -0600

    Further documentation and Button Icon build

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import { AddIcon, CloseIcon } from './components/Icons';
-import { Button } from './components/Button';
+import { AddIcon, CloseIcon, DeleteIcon } from './components/Icons';
+import { Button, ButtonIcon } from './components/Button';
 
 function App() {
   return (
     <>
-      <AddIcon classes='rect-80' />
+      <h1>Hello World</h1>
       <div className='flex items-center space-x-16 mt-32'>
         <Button startIcon={<AddIcon />}>Label</Button>
         <Button variant='outlined' endIcon={<CloseIcon />}>
           Label
         </Button>
+        <Button size='sm' endIcon={<CloseIcon />}>
+          Label
+        </Button>
+        <Button size='lg' endIcon={<CloseIcon />}>
+          Label
+        </Button>
+        <ButtonIcon size='xs' icon={<AddIcon />} />
+        <ButtonIcon size='sm' icon={<AddIcon />} />
+        <ButtonIcon size='md' icon={<AddIcon />} />
+        <ButtonIcon size='lg' icon={<AddIcon />} />
       </div>
     </>
   );
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -7,13 +7,22 @@ export type IconProps = {
   classes?: string;
 
   /**
+   Specify whether the icon is outlined or filled
+   */
+  outlined?: boolean;
+
+  /**
    The root node component.
    */
   children?: React.ReactNode;
 };
 
 // Wrapper component
-const Icon = ({ classes = 'rect-24', children }: IconProps) => {
+const Icon = ({
+  classes = 'rect-24',
+  outlined = false,
+  children,
+}: IconProps) => {
   return (
     <svg
       aria-hidden='true'
@@ -39,6 +48,26 @@ export const CloseIcon: React.FC<IconProps> = ({ ...props }) => {
   return (
     <Icon {...props}>
       <path d='M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z' />
+    </Icon>
+  );
+};
+
+export const DeleteIcon: React.FC<IconProps> = ({ ...props }) => {
+  return (
+    <Icon {...props}>
+      {props.outlined ? (
+        <path d='M16 9V19H8V9H16ZM14.5 3H9.5L8.5 4H5V6H19V4H15.5L14.5 3ZM18 7H6V19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7Z' />
+      ) : (
+        <path d='M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM19 4H15.5L14.5 3H9.5L8.5 4H5V6H19V4Z' />
+      )}
+    </Icon>
+  );
+};
+
+export const ArrowRightIcon: React.FC<IconProps> = ({ ...props }) => {
+  return (
+    <Icon {...props}>
+      <path d='M12 4L10.59 5.41L16.17 11H4V13H16.17L10.59 18.59L12 20L20 12L12 4Z' />
     </Icon>
   );
 };
